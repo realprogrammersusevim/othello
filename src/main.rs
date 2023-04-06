4e1c0889f9cbc53dc0c39fce76a5e89ef7ff89ab
Jonathan <git@jonathansm.dev>
2023-04-06 13:28:28 -0500

    feat(ui): Better user interface in terminal raw mode
    Made the user interface be in raw mode to accept move options from the
    user.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::io::{self, stdout, Write};
+use std::io::{self, stdin, stdout, Write};
+use termion::event::{Event, Key};
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
 const BOARD_SIZE: usize = 8;
@@ -29,8 +31,14 @@ fn main() {
     let mut current_player = Square::White;
     for _ in 1..60 {
         let moves = find_valid_moves(&board, current_player);
-        let best_move = find_best_move(&board, current_player, moves.clone());
-        update_board(&mut board, best_move.0, best_move.1, current_player);
+        let mut next_move: (usize, usize) = (0, 0);
+        if current_player == Square::Black {
+            next_move = find_best_move(&board, current_player, moves.clone());
+        } else if current_player == Square::White {
+            next_move = get_user_move(moves);
+        }
+
+        update_board(&mut board, next_move.0, next_move.1, current_player);
         render_board(&board);
 
         current_player = match current_player {
@@ -176,44 +184,51 @@ fn find_best_move(board: &Board, player: Square, moves: Vec<(usize, usize)>) -> 
     for one_valid in moves {
         let mut current_score = 0;
         let mut board_copy = board.to_owned();
-        current_score += count_pieces(&board_copy, player);
         update_board(&mut board_copy, one_valid.1, one_valid.0, player);
+        // current_score += count_pieces(&board_copy, player);
 
         for two_valid in find_valid_moves(&board_copy, player) {
             let mut board_copy2 = board_copy.to_owned();
-            current_score += count_pieces(&board_copy2, player);
             update_board(&mut board_copy2, two_valid.1, two_valid.0, player);
+            // current_score += count_pieces(&board_copy2, player);
 
             for three_valid in find_valid_moves(&board_copy2, player) {
                 let mut board_copy3 = board_copy2.to_owned();
-                current_score += count_pieces(&board_copy3, player);
                 update_board(&mut board_copy3, three_valid.1, three_valid.0, player);
+                // current_score += count_pieces(&board_copy3, player);
 
                 for four in find_valid_moves(&board_copy3, player) {
                     let mut board_copy4 = board_copy3.to_owned();
-                    current_score += count_pieces(&board_copy4, player);
                     update_board(&mut board_copy4, four.1, four.0, player);
+                    // current_score += count_pieces(&board_copy4, player);
 
                     for five in find_valid_moves(&board_copy4, player) {
                         let mut board_copy5 = board_copy4.to_owned();
-                        current_score += count_pieces(&board_copy5, player);
                         update_board(&mut board_copy5, five.1, five.0, player);
+                        // current_score += count_pieces(&board_copy5, player);
 
                         for six in find_valid_moves(&board_copy5, player) {
                             let mut board_copy6 = board_copy5.to_owned();
-                            current_score += count_pieces(&board_copy6, player);
                             update_board(&mut board_copy6, six.1, six.0, player);
+                            // current_score += count_pieces(&board_copy6, player);
 
                             for seven in find_valid_moves(&board_copy6, player) {
                                 let mut board_copy7 = board_copy6.to_owned();
-                                current_score += count_pieces(&board_copy7, player);
                                 update_board(&mut board_copy7, seven.1, seven.0, player);
+                                // current_score += count_pieces(&board_copy7, player);
+
+                                for eight in find_valid_moves(&board_copy7, player) {
+                                    let mut board_copy8 = board_copy7.to_owned();
+                                    update_board(&mut board_copy8, eight.0, eight.1, player);
+                                    current_score += count_pieces(&board_copy7, player);
+                                }
                             }
                         }
                     }
                 }
             }
         }
+
         if current_score > best_score {
             best_move = one_valid;
             best_score = current_score;
@@ -221,4 +236,52 @@ fn find_best_move(board: &Board, player: Square, moves: Vec<(usize, usize)>) -> 
     }
 
     best_move
+}
+
+fn get_user_move(moves: Vec<(usize, usize)>) -> (usize, usize) {
+    let labels = vec![
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "q", "w", "e", "r", "t", "y", "u", "i",
+        "o", "p", "a", "s", "d", "f",
+    ];
+
+    for i in 0..moves.len() {
+        print!("{}", termion::clear::CurrentLine);
+        println!("{}: ({}, {})  ", labels[i], moves[i].0, moves[i].1);
+    }
+    stdout().flush().unwrap();
+
+    loop {
+        for event in stdin().events() {
+            match event.unwrap() {
+                Event::Key(key) => match key {
+                    Key::Char('0') => return moves[0],
+                    Key::Char('1') => return moves[1],
+                    Key::Char('2') => return moves[2],
+                    Key::Char('3') => return moves[3],
+                    Key::Char('4') => return moves[4],
+                    Key::Char('5') => return moves[5],
+                    Key::Char('6') => return moves[6],
+                    Key::Char('7') => return moves[7],
+                    Key::Char('8') => return moves[8],
+                    Key::Char('9') => return moves[9],
+                    Key::Char('q') => return moves[10],
+                    Key::Char('w') => return moves[11],
+                    Key::Char('e') => return moves[12],
+                    Key::Char('r') => return moves[13],
+                    Key::Char('t') => return moves[14],
+                    Key::Char('y') => return moves[15],
+                    Key::Char('u') => return moves[16],
+                    Key::Char('i') => return moves[17],
+                    Key::Char('o') => return moves[18],
+                    Key::Char('p') => return moves[19],
+                    Key::Char('a') => return moves[20],
+                    Key::Char('s') => return moves[21],
+                    Key::Char('d') => return moves[22],
+                    Key::Char('f') => return moves[23],
+                    _ => continue,
+                },
+                _ => {}
+            }
+        }
+    }
 }
