a8d30f781afc26f23957466e6520fc7b9ab000d6
Jonathan <git@jonathansm.dev>
2026-04-08 15:56:10 -0500

    refactor: use rayon to parallelize and reorganize code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use rayon::prelude::*;
 use std::io::{self, stdin, stdout, Write};
+use std::sync::{Arc, Mutex};
 use termion::event::{Event, Key};
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
@@ -6,60 +8,64 @@ use termion::raw::IntoRawMode;
 const BOARD_SIZE: usize = 8;
 
 #[derive(Copy, Clone, PartialEq)]
-enum Square {
+enum SquareState {
     Empty,
     White,
     Black,
 }
 
-type Board = [[Square; BOARD_SIZE]; BOARD_SIZE];
+type Board = [[SquareState; BOARD_SIZE]; BOARD_SIZE];
 
 fn main() {
     print!("{}", termion::clear::All);
     stdout().into_raw_mode().unwrap();
 
-    let mut board: Board = [[Square::Empty; BOARD_SIZE]; BOARD_SIZE];
+    let mut board: Board = [[SquareState::Empty; BOARD_SIZE]; BOARD_SIZE];
 
     // Set up initial state
-    board[3][3] = Square::White;
-    board[3][4] = Square::Black;
-    board[4][3] = Square::Black;
-    board[4][4] = Square::White;
+    board[3][3] = SquareState::White;
+    board[3][4] = SquareState::Black;
+    board[4][3] = SquareState::Black;
+    board[4][4] = SquareState::White;
 
-    update_board(&mut board, 2, 3, Square::Black);
+    update_board(&mut board, 2, 3, SquareState::Black);
     render_board(&board);
-    let mut current_player = Square::White;
+    let mut current_player = SquareState::White;
     for _ in 1..60 {
         let moves = find_valid_moves(&board, current_player);
-        let mut next_move: (usize, usize) = (0, 0);
-        if current_player == Square::Black {
-            next_move = find_best_move(&board, current_player, moves.clone());
-        } else if current_player == Square::White {
-            next_move = get_user_move(moves);
-        }
+        let next_move: (usize, usize) = match current_player {
+            SquareState::White => get_user_move(moves),
+            SquareState::Black => find_best_move(&board, current_player, moves.clone()),
+            _ => (0, 0),
+        };
 
         update_board(&mut board, next_move.0, next_move.1, current_player);
         render_board(&board);
 
         current_player = match current_player {
-            Square::White => Square::Black,
-            Square::Black => Square::White,
-            _ => Square::Empty,
+            SquareState::White => SquareState::Black,
+            SquareState::Black => SquareState::White,
+            _ => SquareState::Empty,
         }
     }
 
-    let white = count_pieces(&board, Square::White);
-    let black = count_pieces(&board, Square::Black);
+    let white = count_pieces(&board, SquareState::White);
+    let black = count_pieces(&board, SquareState::Black);
     if white > black {
         println!("White wins with {} pieces!", white);
-    } else if black > white {
-        println!("Black wins with {} pieces!", black);
     } else {
-        println!("Draw!");
+        match black > white {
+            true => {
+                println!("Black wins with {} pieces!", black);
+            }
+            false => {
+                println!("Draw!");
+            }
+        }
     }
 }
 
-fn update_board(board: &mut Board, x: usize, y: usize, player: Square) {
+fn update_board(board: &mut Board, x: usize, y: usize, player: SquareState) {
     board[y][x] = player;
     let mut k: usize;
     let mut l: usize;
@@ -89,7 +95,7 @@ fn update_board(board: &mut Board, x: usize, y: usize, player: Square) {
                     }
                     break;
                 }
-                if board[k][l] == Square::Empty {
+                if board[k][l] == SquareState::Empty {
                     break;
                 }
             }
@@ -97,17 +103,17 @@ fn update_board(board: &mut Board, x: usize, y: usize, player: Square) {
     }
 }
 
-fn find_valid_moves(board: &Board, player: Square) -> Vec<(usize, usize)> {
+fn find_valid_moves(board: &Board, player: SquareState) -> Vec<(usize, usize)> {
     let mut moves: Vec<(usize, usize)> = Vec::new();
     let opponent = match player {
-        Square::White => Square::Black,
-        Square::Black => Square::White,
-        _ => Square::Empty,
+        SquareState::White => SquareState::Black,
+        SquareState::Black => SquareState::White,
+        _ => SquareState::Empty,
     };
 
     for i in 0..BOARD_SIZE {
         for j in 0..BOARD_SIZE {
-            if board[i][j] != Square::Empty {
+            if board[i][j] != SquareState::Empty {
                 continue;
             }
             for x in -1..=1 {
@@ -141,101 +147,103 @@ fn find_valid_moves(board: &Board, player: Square) -> Vec<(usize, usize)> {
     moves
 }
 
-fn count_pieces(board: &Board, player: Square) -> usize {
+fn count_pieces(board: &Board, player: SquareState) -> usize {
     let mut player_count = 0;
 
-    for i in 0..BOARD_SIZE {
+    (0..BOARD_SIZE).for_each(|i| {
         for j in 0..BOARD_SIZE {
             if board[i][j] == player {
                 player_count += 1;
             }
         }
-    }
+    });
 
     player_count
 }
 
 fn render_board(board: &Board) {
+    print!("{}", termion::clear::All);
     print!("{}", termion::cursor::Goto(1, 1));
     print!("{}", termion::clear::CurrentLine);
     println!("  0 1 2 3 4 5 6 7");
-    for i in 0..BOARD_SIZE {
+    (0..BOARD_SIZE).for_each(|i| {
         print!("{}", termion::clear::CurrentLine);
         print!("{} ", i);
         for j in 0..BOARD_SIZE {
             let symbol = match board[i][j] {
-                Square::White => "\u{25cf} ", // black piece
-                Square::Black => "\u{25cb} ", // white piece
-                Square::Empty => "\u{00b7} ", // empty cell
+                SquareState::White => "\u{25cf} ", // black piece
+                SquareState::Black => "\u{25cb} ", // white piece
+                SquareState::Empty => "\u{00b7} ", // empty cell
             };
             print!("{}", symbol);
         }
         println!("{}", i);
-    }
+    });
     print!("{}", termion::clear::CurrentLine);
     println!("  0 1 2 3 4 5 6 7");
     io::stdout().flush().unwrap();
 }
 
-fn find_best_move(board: &Board, player: Square, moves: Vec<(usize, usize)>) -> (usize, usize) {
-    let mut best_score = 0;
-    let mut best_move: (usize, usize) = moves[0];
+const MAX_DEPTH: usize = 12;
 
-    for one_valid in moves {
-        let mut current_score = 0;
-        let mut board_copy = board.to_owned();
-        update_board(&mut board_copy, one_valid.1, one_valid.0, player);
-        // current_score += count_pieces(&board_copy, player);
+fn find_best_move(
+    board: &Board,
+    player: SquareState,
+    moves: Vec<(usize, usize)>,
+) -> (usize, usize) {
+    // TODO: implement alpha-beta search pruning so we can avoid spending time on
+    // bad branches and search the good branches deeper.
+    let best_score = Arc::new(Mutex::new(i32::MIN));
+    let best_move = Arc::new(Mutex::new(moves[0]));
 
-        for two_valid in find_valid_moves(&board_copy, player) {
-            let mut board_copy2 = board_copy.to_owned();
-            update_board(&mut board_copy2, two_valid.1, two_valid.0, player);
-            // current_score += count_pieces(&board_copy2, player);
+    moves.par_iter().for_each(|&mov| {
+        let score = score_move(
+            board,
+            player,
+            mov,
+            MAX_DEPTH,
+            &mut best_score.lock().unwrap().clone(),
+        );
 
-            for three_valid in find_valid_moves(&board_copy2, player) {
-                let mut board_copy3 = board_copy2.to_owned();
-                update_board(&mut board_copy3, three_valid.1, three_valid.0, player);
-                // current_score += count_pieces(&board_copy3, player);
+        let mut best_score_guard = best_score.lock().unwrap();
+        let mut best_move_guard = best_move.lock().unwrap();
 
-                for four in find_valid_moves(&board_copy3, player) {
-                    let mut board_copy4 = board_copy3.to_owned();
-                    update_board(&mut board_copy4, four.1, four.0, player);
-                    // current_score += count_pieces(&board_copy4, player);
-
-                    for five in find_valid_moves(&board_copy4, player) {
-                        let mut board_copy5 = board_copy4.to_owned();
-                        update_board(&mut board_copy5, five.1, five.0, player);
-                        // current_score += count_pieces(&board_copy5, player);
-
-                        for six in find_valid_moves(&board_copy5, player) {
-                            let mut board_copy6 = board_copy5.to_owned();
-                            update_board(&mut board_copy6, six.1, six.0, player);
-                            // current_score += count_pieces(&board_copy6, player);
-
-                            for seven in find_valid_moves(&board_copy6, player) {
-                                let mut board_copy7 = board_copy6.to_owned();
-                                update_board(&mut board_copy7, seven.1, seven.0, player);
-                                // current_score += count_pieces(&board_copy7, player);
-
-                                for eight in find_valid_moves(&board_copy7, player) {
-                                    let mut board_copy8 = board_copy7.to_owned();
-                                    update_board(&mut board_copy8, eight.0, eight.1, player);
-                                    current_score += count_pieces(&board_copy7, player);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+        if score > *best_score_guard {
+            *best_score_guard = score;
+            *best_move_guard = mov;
         }
+    });
 
-        if current_score > best_score {
-            best_move = one_valid;
-            best_score = current_score;
-        }
+    let x = *best_move.lock().unwrap();
+    x
+}
+
+fn score_move(
+    board: &Board,
+    player: SquareState,
+    next_move: (usize, usize),
+    depth: usize,
+    best_score: &mut i32,
+) -> i32 {
+    if depth == 0 {
+        return count_pieces(board, player) as i32;
     }
 
-    best_move
+    let mut board_copy = board.to_owned();
+    update_board(&mut board_copy, next_move.0, next_move.1, player);
+    let next_valid_moves = find_valid_moves(&board_copy, player);
+
+    let mut current_score = 0;
+    for next_next_move in next_valid_moves {
+        current_score += score_move(&board_copy, player, next_next_move, depth - 1, best_score);
+    }
+
+    if depth == MAX_DEPTH && current_score > *best_score {
+        *best_score = current_score;
+        return current_score;
+    }
+
+    current_score
 }
 
 fn get_user_move(moves: Vec<(usize, usize)>) -> (usize, usize) {
@@ -252,8 +260,8 @@ fn get_user_move(moves: Vec<(usize, usize)>) -> (usize, usize) {
 
     loop {
         for event in stdin().events() {
-            match event.unwrap() {
-                Event::Key(key) => match key {
+            if let Event::Key(key) = event.unwrap() {
+                match key {
                     Key::Char('0') => return moves[0],
                     Key::Char('1') => return moves[1],
                     Key::Char('2') => return moves[2],
@@ -279,8 +287,7 @@ fn get_user_move(moves: Vec<(usize, usize)>) -> (usize, usize) {
                     Key::Char('d') => return moves[22],
                     Key::Char('f') => return moves[23],
                     _ => continue,
-                },
-                _ => {}
+                }
             }
         }
     }
